d85fb05011e2cb4f23bce6959c450d15830ebb30
Ignacio Salcedo <ignacio@isalcedo.com>
2017-07-18 15:41:19 -0500

    Update trumbowyg.colors.js
    Spanish Locale

--- plugins/colors/trumbowyg.colors.js
+++ plugins/colors/trumbowyg.colors.js
@@ -22,6 +22,10 @@
                 foreColor: 'Text color',
                 backColor: 'Background color'
             },
+            es: {
+                foreColor: 'Color del texto',
+                backColor: 'Color de fondo'
+            },
             fr: {
                 foreColor: 'Couleur du texte',
                 backColor: 'Couleur de fond'
